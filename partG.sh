f4df791934388ab8f7c76dbfa0edfd10b8514124
stephenseiber <s.w_S@me.com>
2020-11-02 11:36:30 -0700

    Delete partG.sh

--- partG.sh
+++
@@ -1,6 +0,0 @@
-cp /arch-install/makepkgN.conf /etc/makepkg.conf 
-pacman -S - </packageN.txt --noconfirm
-cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf
-cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook
-cp /arch-install/archN.conf /boot/loader/entries/arch.conf
-mkinitcpio -p linux
