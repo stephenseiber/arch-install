cd08f08ced9b607d1ca055e0560761bd2fe6f145
stephenseiber <s.w_S@me.com>
2020-11-02 09:01:44 -0700

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -16,4 +16,15 @@ chsh -s /bin/fish
 pacman-key --init
 pacman-key --populate archlinux
 cp /arch-install/sudoers /etc/sudoers
-
+echo name computer
+read hostname
+echo name user
+read user
+sudo useradd -m -g users -G wheel -s /bin/fish $user
+echo "what do you want your password to be?"
+passwd $user
+echo "what do you want root passwd to be"
+passwd
+echo passwd set
+useradd -m -g users -G wheel -s /bin/fish $user
+echo "${hostname}" > /etc/hostname
