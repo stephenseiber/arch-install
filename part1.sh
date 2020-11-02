7c7cbf9a011f584f38a8228fa7ee37f0f2154b28
stephenseiber <s.w_S@me.com>
2020-11-01 17:42:27 -0700

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -1,9 +1,10 @@
 mount -o remount,size=4G /run/archiso/cowspace
 df -l
 pacman -Sy reflector fish --noconfirm
-fish
+
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 reflector --verbose -l 10 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy
 lsblk
+fish
