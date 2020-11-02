b29a0e19b2b4f501c4e0f322ec129e85d6267c93
stephenseiber <s.w_S@me.com>
2020-11-01 19:23:48 -0700

    Update partG.sh

--- partG.sh
+++ partG.sh
@@ -1,4 +1,4 @@
-cp /arch-install/makepkgN /etc/makepkg.conf 
+cp /arch-install/makepkgN.conf /etc/makepkg.conf 
 pacman -S - </packageN.txt --noconfirm
 cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf
 cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook
