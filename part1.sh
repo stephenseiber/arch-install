73b5b5e03218f50b8b0a7a54437fa4d4ed2d46fb
stephenseiber <s.w_S@me.com>
2020-11-02 11:34:49 -0700

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -25,3 +25,15 @@ e2label "$pb" boot
 mount $pr /mnt
 mkdir -p /mnt/boot
 mount $pb /mnt/boot
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=1024 status=progress
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
+pacstrap /mnt fish pacman git nano
+genfstab -U /mnt >> /mnt/etc/fstab
+cp -r ~/arch-install /mnt/arch-install
+cp pacman.conf /mnt/etc/pacman.conf
+cp package.txt /mnt/package.txt
+cp packageN.txt /mnt/packageN.txt
+reflector --verbose -l 10 --sort rate --save /mnt/etc/pacman.d/mirrorlist
+arch-chroot /mnt /bin/fish
