b90fafc84227b102892b4619a23975e04be03f4e
stephenseiber <s.w_S@me.com>
2020-11-01 17:38:50 -0700

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -4,9 +4,9 @@ mkswap /mnt/swapfile
 swapon /mnt/swapfile
 pacstrap /mnt fish pacman git nano
 genfstab -U /mnt >> /mnt/etc/fstab
-cp /arch-install /mnt/arch-install
-cp /arch-install/pacman.conf /mnt/etc/pacman.conf
-cp /arch-install/package.txt /mnt/package.txt
-cp /arch-install/packageN.txt /mnt/packageN.txt
+cp ~/arch-install /mnt/arch-install
+cp pacman.conf /mnt/etc/pacman.conf
+cp package.txt /mnt/package.txt
+cp packageN.txt /mnt/packageN.txt
 reflector --verbose -l 10 --sort rate --save /mnt/etc/pacman.d/mirrorlist
 arch-chroot /mnt /bin/fish
