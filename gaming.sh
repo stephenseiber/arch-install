c931be68610f6ff9cdce80defb3d2cf1704227ca
stephenseiber <s.w_S@me.com>
2020-11-01 18:43:21 -0700

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -10,3 +10,7 @@ cd ..
 rm -Rf optimus-manager-qt
 sudo systemctl enable optimus-manager
 sudo systemctl start optimus-manager
+sudo pacman -Sy arch-install-scripts
+rm -R /etc/fstab
+genfstab -U / >> /etc/fstab
+pacman -Rs arch-install-scripts
