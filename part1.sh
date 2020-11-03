0d0e06365de358323e65bfe535ec791325e26629
stephenseiber <s.w_S@me.com>
2020-11-03 14:03:38 -0700

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -37,3 +37,4 @@ cp package.txt /mnt/package.txt
 cp packageN.txt /mnt/packageN.txt
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 arch-chroot /mnt /bin/bash /arch-install/part2.sh
+reboot
