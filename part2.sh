283cc4427756f7ac2e4bfedcc60d1c4b072e70b8
stephenseiber <s.w_S@me.com>
2020-11-03 14:03:01 -0700

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -2,7 +2,7 @@ cp /arch-install/locale.gen /etc/locale.gen
 locale-gen
 ln -s /usr/share/zoneinfo/America/Denver /etc/localtime
 pacman -Syyu - </package.txt --noconfirm
-
+rm -r /package.txt
 hwclock --systohc --utc
 mkdir -p /etc/pacman.d/hooks
 cp /arch-install/100-systemd-boot.hook /etc/pacman.d/hooks/100-systemd-boot.hook
@@ -31,20 +31,19 @@ echo "${hostname}" > /etc/hostname
 mkdir -p /home/$user/
 mkdir -p /home/$user/Desktop
 cd /home/$user/
-cp /arch-install/yay.sh yay.sh
-cp /arch-install/gaming.sh gaming.sh
-cp /arch-install/home.tar.gz home.tar.gz
-tar -xvf home.tar.gz 
-rm -r home.tar.gz
-chown -R $user /home/$user
 echo first not root drive name? if not on gaming computer type no
 read drivea
 if [ $drivea == 'no' ] ;
     then echo "done";
+    cp /arch-install/yay.sh yay.sh
     cp /arch-install/makepkg /etc/makepkg.conf;
-    cp /arch-install/arch.conf /boot/loader/entries/arch.conf;
-    rm -r /home/$user/.config/autostart/io.optimus_manager.OptimusManagerQt.desktop;
+    cp /arch-install/arch.conf /boot/loader/entries/arch.conf
+    cp /arch-install/home.tar.gz home.tar.gz;
+    tar -xvf home.tar.gz;
+    rm -r home.tar.gz
+    chown -R $user /home/$user
     /bin/su -c "sh yay.sh" - $user;
+    rm -r yay.sh
     else mkdir -p /home/$user//Desktop/$drivea
     devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac) ;
     devicea=$(dialog --stdout --menu "Select drivea" 0 0 0 ${devicelist}) ;
@@ -56,8 +55,16 @@ if [ $drivea == 'no' ] ;
         echo "UUID=$uuid       /home/$user/Desktop/$drivea     ext4            rw,relatime     0 2" >> /etc/fstab;
         mount $parta /home/$user//Desktop/$drivea;
         cp /arch-install/makepkgN.conf /etc/makepkg.conf;
+        cp /arch-install/homeN.tar.gz home.tar.gz;
+        cp /arch-install/yay.sh yay.sh
+        cp /arch-install/gaming.sh gaming.sh
+        tar -xvf home.tar.gz;
+        rm -r home.tar.gz
+        chown -R $user /home/$user
         /bin/su -c "sh yay.sh" - $user;
         /bin/su -c "sh gaming.sh" - $user;
+        rm -r yay.sh
+        rm -r gaming.sh
         pacman -S - </packageN.txt --noconfirm;
         cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
         cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
@@ -72,6 +79,7 @@ if [ $drivea == 'no' ] ;
         /bin/su -c "sh gaming.sh" - $user;
         cp /arch-install/makepkgN.conf /etc/makepkg.conf; 
         pacman -S - </packageN.txt --noconfirm;
+        rm -r /packageN.txt
         cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
         cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
         cp /arch-install/archN.conf /boot/loader/entries/arch.conf;
@@ -108,5 +116,6 @@ echo this is still working
 echo thumbs up
 chown -R $user /home/$user
 clear
+rm -r /arch-install
 echo done
 
