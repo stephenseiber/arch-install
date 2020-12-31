d739bbf539cc90319ea996e2735a1f575bba9746
stephenseiber <s.w_S@me.com>
2020-12-31 10:26:07 -0700

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -8,15 +8,5 @@ cd kalu-kde
 makepkg -si --noconfirm
 cd ..
 rm -Rf kalu-kde
-git clone https://aur.archlinux.org/jre8.git
-cd jre8
-makepkg -si --noconfirm
-cd ..
-rm -Rf jre8
-git clone https://aur.archlinux.org/minecraft-launcher.git
-cd minecraft-launcher
-makepkg -si --noconfirm
-cd ..
-rm -Rf minecraft-launcher
 
 
