9e04db6d6e1dcdf9d59ce1d97f0be51a51c669e7
stephenseiber <s.w_S@me.com>
2021-01-07 17:51:47 -0700

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -29,22 +29,33 @@ passwd
 echo passwd set
 echo "${hostname}" > /etc/hostname
 mkdir -p /home/$user/
-mkdir -p /home/$user/Desktop
 cd /home/$user/
-echo first not root drive name? if not on gaming computer type no
-read drivea
-if [ $drivea == 'no' ] ;
-    then echo "done";
-    cp /arch-install/yay.sh yay.sh
-    cp /arch-install/makepkg /etc/makepkg.conf;
-    cp /arch-install/arch.conf /boot/loader/entries/arch.conf
-    #cp /arch-install/home.tar.gz home.tar.gz;
-    #tar -xvf home.tar.gz;
-    #rm -r home.tar.gz
-    chown -R $user /home/$user
-    /bin/su -c "sh yay.sh" - $user;
-    rm -r yay.sh
-    else mkdir -p /home/$user//Desktop/$drivea
+cp /arch-install/yay.sh yay.sh
+/bin/su -c "sh yay.sh" - $user;
+rm -r yay.sh
+echo is this gaming laptop
+read gamer
+if [ $gamer == no ]
+then
+cp /arch-install/makepkg /etc/makepkg.conf;
+cp /arch-install/arch.conf /boot/loader/entries/arch.conf;
+    else
+    cp /arch-install/makepkgN.conf /etc/makepkg.conf;
+    cp /arch-install/nvidia-drm.conf /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf
+    cp /arch-install/xinitrc /home/$user/.xinitrc
+    cp /arch-install/sddm /usr/share/sddm/scripts/Xsetup
+    cp /arch-install/homeN.tar.gz home.tar.gz;
+    pacman -S - </packageN.txt --noconfirm;
+    pip install requests vdf;
+    sudo pacman -R amdvlk lib32-amdvlk --noconfirm;
+    cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
+    cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
+    cp /arch-install/archN.conf /boot/loader/entries/arch.conf;        
+    rm -r /packageN.txt;
+    mkinitcpio -p linux;
+    echo first not root drive name?
+    read drivea
+    mkdir -p /home/$user/$drivea
     devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac) ;
     devicea=$(dialog --stdout --menu "Select drivea" 0 0 0 ${devicelist}) ;
     if [ ${devicea} == '/dev/sdb' ] || [ ${devicea} == '/dev/sda' ] ;
@@ -52,50 +63,21 @@ if [ $drivea == 'no' ] ;
         echo $parta is selected drive;
         uuid=$(blkid -o value -s UUID $parta);
         echo $uuid;
-        echo "UUID=$uuid       /home/$user/Desktop/$drivea     ext4            rw,relatime     0 2" >> /etc/fstab;
-        mount $parta /home/$user//Desktop/$drivea;
-        cp /arch-install/makepkgN.conf /etc/makepkg.conf;
-        cp /arch-install/homeN.tar.gz home.tar.gz;
-        cp /arch-install/yay.sh yay.sh;
-        #cp /arch-install/gaming.sh gaming.sh;
-        #tar -xvf home.tar.gz;
-        #rm -r home.tar.gz;
-        chown -R $user /home/$user;
-        /bin/su -c "sh yay.sh" - $user;
-        #/bin/su -c "sh gaming.sh" - $user;
-        rm -r yay.sh;
-        rm -r gaming.sh;
-        pacman -S - </packageN.txt --noconfirm;
-        pip install requests vdf;
-        sudo pacman -R amdvlk lib32-amdvlk --noconfirm;
-        cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
-        cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
-        cp /arch-install/archN.conf /boot/loader/entries/arch.conf;
-        mkinitcpio -p linux;
+        echo "UUID=$uuid       /home/$user/$drivea     ext4            rw,relatime     0 2" >> /etc/fstab;
+        mount $parta /home/$user/$drivea;
         else parta="${devicea}p1";
         echo $parta is selected drive;
-        mount $parta /home/$user//Desktop/$drivea;
+        mount $parta /home/$user//$drivea;
         uuid=$(blkid -o value -s UUID $parta);
         echo $uuid;
-        echo "UUID=$uuid       /home/$user/Desktop/$drivea     ext4            rw,relatime     0 2" >> /etc/fstab;
-        #/bin/su -c "sh gaming.sh" - $user;
-        cp /arch-install/makepkgN.conf /etc/makepkg.conf; 
-        pacman -S - </packageN.txt --noconfirm;
-        rm -r /packageN.txt
-        cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
-        cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
-        cp /arch-install/archN.conf /boot/loader/entries/arch.conf;
-        mkinitcpio -p linux;
-    fi
-fi
+        echo "UUID=$uuid       /home/$user/$drivea     ext4            rw,relatime     0 2" >> /etc/fstab;
 
-if [ $drivea == 'no' ] ;
-    then echo "done" ;
-    else echo second not root drive name? if no second non root drive type no;
+    fi
+    echo second non root drive name? if no second non root drive type no;
     read driveb
         if [ $driveb == 'no' ] ;
-        then echo "done" ;/bin/su -c "sh yay.sh" - $user
-        else mkdir -p /home/$user//Desktop/$driveb;
+        then echo "done" 
+        else mkdir -p /home/$user/$driveb;
         devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac) ;
         deviceb=$(dialog --stdout --menu "Select drivea" 0 0 0 ${devicelist}) ;
             if [ ${deviceb} == '/dev/sdb' ] || [ ${deviceb} == '/dev/sda' ] ;
@@ -103,14 +85,14 @@ if [ $drivea == 'no' ] ;
             uuid=$(blkid -o value -s UUID $partb);
             echo $uuid;
             echo $partb is selected drive;
-            mount $partb /home/$user/Desktop/$driveb;
-            echo "$UUID=$uuid       /home/$user/Desktop/$driveb     ext4            rw,relatime     0 2" >> /etc/fstab;
+            mount $partb /home/$user/$driveb;
+            echo "$UUID=$uuid       /home/$user/$driveb     ext4            rw,relatime     0 2" >> /etc/fstab;
             else partb="${deviceb}p1";
             echo $partb is selected drive;
-            mount $partb /home/$user//Desktop/$driveb;
+            mount $partb /home/$user/$driveb;
             uuid=$(blkid -o value -s UUID $partb);
             echo $uuid;
-            echo "UUID=$uuid       /home/$user/Desktop/$driveb     ext4            rw,relatime     0 2" >> /etc/fstab;
+            echo "UUID=$uuid       /home/$user/$driveb     ext4            rw,relatime     0 2" >> /etc/fstab;
         fi
     fi
 fi
