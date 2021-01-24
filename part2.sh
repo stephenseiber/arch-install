c5b97d0df3d2f1970a1f64a1a7b7077fd60a1cc3
stephenseiber <s.w_S@me.com>
2021-01-24 14:07:07 -0500

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -43,7 +43,6 @@ cp /arch-install/arch.conf /boot/loader/entries/arch.conf;
     cp /arch-install/makepkgN.conf /etc/makepkg.conf;
     pacman -S - </packageN.txt --noconfirm;
     pip install requests vdf;
-    sudo pacman -R amdvlk lib32-amdvlk --noconfirm;
     cp /arch-install/mkinitcpio.conf /etc/mkinitcpio.conf;
     cp /arch-install/nvidia.hook /etc/pacman.d/hooks/nvidia.hook;
     cp /arch-install/archN.conf /boot/loader/entries/arch.conf;        
