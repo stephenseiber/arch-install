0ef6ffaa935af304a8d272ad6c0465e01f643ae3
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 19:09:41 -0600

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,11 @@
+echo name computer
+read hostname
+echo "${hostname}" > /etc/hostname
+echo name user
+read user
+useradd -m -g users -G wheel -s /bin/fish $user
+echo "what do you want your password to be?"
+passwd $user
+echo "what do you want root passwd to be"
+passwd
+echo passwd set
