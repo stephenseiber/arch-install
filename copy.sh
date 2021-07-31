b9d193644d153a589e70d1438205309cea0ac370
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 19:09:11 -0600

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,13 @@
+cp pacman.conf /mnt/etc/pacman.conf
+cp package.txt /mnt/package.txt
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+cp /arch-install/locale.gen /mnt/etc/locale.gen
+mkdir -p /mnt/etc/pacman.d/hooks
+cp ~/arch-install/100-systemd-boot.hook /mnt/etc/pacman.d/hooks/100-systemd-boot.hook
+cp ~/arch-install/loader.conf /mnt/boot/loader/loader.conf
+cp ~/arch-install/reflector.conf /mnt/etc/xdg/reflector/reflector.conf
+cp ~/arch-install/sudoers /mnt/etc/sudoers
+cp ~/arch-install/makepkgN.conf /mnt/etc/makepkg.conf
+cp ~/arch-install/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
+cp ~/arch-install/nvidia.hook /mnt/etc/pacman.d/hooks/nvidia.hook
+cp ~/arch-install/archN.conf /mnt/boot/loader/entries/arch.conf
