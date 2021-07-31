7dbb9b6547e13ef4f728d633694e15e66a7ea2a4
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 19:28:14 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -4,9 +4,6 @@ pacman -Sy reflector --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 reflector --latest 50 --verbose  --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-sh format.sh
-sh pacstrap.sh
-genfstab -U /mnt >> /mnt/etc/fstab
-sh copy.sh
-arch-chroot /mnt /bin/bash /arch-install/part2.sh
+sh /sh/format.sh
+
 reboot
