2b080975a3f179f9380e47ef4d029822b9a427ff
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 19:10:59 -0600

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -4,27 +4,9 @@ pacman -Sy reflector --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 reflector --latest 50 --verbose  --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-parted --script /dev/nvme0n1 -- mklabel gpt \
-  mkpart ESP fat32 1Mib 275MiB \
-  set 1 boot on \
-  mkpart primary ext4 275MiB 100%
-wipefs /dev/nvme0n1p1
-wipefs /dev/nvme0n1p2
-mkfs.vfat -F32 /dev/nvme0n1p1
-mkfs.ext4 /dev/nvme0n1p2
-e2label /dev/nvme0n1p2 arch
-mount /dev/nvme0n1p2 /mnt
-mkdir -p /mnt/boot
-mount /dev/nvme0n1p1 /mnt/boot
-dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048 status=progress
-chmod 600 /mnt/swapfile
-mkswap /mnt/swapfile
-swapon /mnt/swapfile
-pacstrap /mnt fish pacman git nano
+sh format.sh
+sh pacstrap.sh
 genfstab -U /mnt >> /mnt/etc/fstab
-cp -r ~/arch-install /mnt/arch-install
-cp pacman.conf /mnt/etc/pacman.conf
-cp package.txt /mnt/package.txt
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+sh copy.sh
 arch-chroot /mnt /bin/bash /arch-install/part2.sh
 reboot
