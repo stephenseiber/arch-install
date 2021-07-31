32c58de4b53be1f7759aca188dd1c5ca255e93b8
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 19:40:37 -0600

    Update copy.sh

--- sh/copy.sh
+++ sh/copy.sh
@@ -1,14 +1,14 @@
-cp pacman.conf /mnt/etc/pacman.conf
+cp copy/pacman.conf /mnt/etc/pacman.conf
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
-cp /arch-install/locale.gen /mnt/etc/locale.gen
+cp copy/locale.gen /mnt/etc/locale.gen
 mkdir -p /mnt/etc/pacman.d/hooks
-cp /copy/100-systemd-boot.hook /mnt/etc/pacman.d/hooks/100-systemd-boot.hook
-cp /copy/loader.conf /mnt/boot/loader/loader.conf
-cp /copy/reflector.conf /mnt/etc/xdg/reflector/reflector.conf
-cp /copy/sudoers /mnt/etc/sudoers
-cp /copy/makepkg.conf /mnt/etc/makepkg.conf
-cp /copy/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
-cp /copy/nvidia.hook /mnt/etc/pacman.d/hooks/nvidia.hook
-cp /copy/arch.conf /mnt/boot/loader/entries/arch.conf
-cp /sh/user.sh /mnt/user.sh
-arch-chroot /mnt /bin/bash /sh/part2.sh
+cp copy/100-systemd-boot.hook /mnt/etc/pacman.d/hooks/100-systemd-boot.hook
+cp copy/loader.conf /mnt/boot/loader/loader.conf
+cp copy/reflector.conf /mnt/etc/xdg/reflector/reflector.conf
+cp copy/sudoers /mnt/etc/sudoers
+cp copy/makepkg.conf /mnt/etc/makepkg.conf
+cp copy/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
+cp copy/nvidia.hook /mnt/etc/pacman.d/hooks/nvidia.hook
+cp copy/arch.conf /mnt/boot/loader/entries/arch.conf
+cp sh/user.sh /mnt/user.sh
+arch-chroot /mnt /bin/bash sh/part2.sh
