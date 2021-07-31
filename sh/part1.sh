b3673d0e83e1c3859dc94a3a58689a8b1ef27da7
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 20:08:54 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -4,6 +4,6 @@ cp copy/pacman.conf /etc/pacman.conf
 pacman -Sy reflector --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
-reflector --latest 5 --verbose  --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 8 --verbose  --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sh sh/format.sh
 #reboot
