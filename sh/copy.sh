1e649a09fd69512ab09ce423f88e8864abfaf00a
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-07-30 20:23:12 -0600

    Update copy.sh

--- sh/copy.sh
+++ sh/copy.sh
@@ -3,13 +3,16 @@ cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp copy/locale.gen /mnt/etc/locale.gen
 mkdir -p /mnt/etc/pacman.d/hooks
 cp copy/100-systemd-boot.hook /mnt/etc/pacman.d/hooks/100-systemd-boot.hook
-cp copy/loader.conf /mnt/boot/loader/loader.conf
+
 cp copy/reflector.conf /mnt/etc/xdg/reflector/reflector.conf
 cp copy/sudoers /mnt/etc/sudoers
 cp copy/makepkg.conf /mnt/etc/makepkg.conf
 cp copy/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
 cp copy/nvidia.hook /mnt/etc/pacman.d/hooks/nvidia.hook
-cp copy/arch.conf /mnt/boot/loader/entries/arch.conf
+
 cp sh/user.sh /mnt/user.sh
 cp sh/part2.sh /mnt/part2.sh
 arch-chroot /mnt /bin/bash /part2.sh
+
+cp copy/loader.conf /mnt/boot/loader/loader.conf
+cp copy/arch.conf /mnt/boot/loader/entries/arch.conf
