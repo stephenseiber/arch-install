18f2d4940ebe1afaf5b0ba43b1ec26584c5ba73e
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-08-01 12:38:21 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,7 +1,5 @@
-mount -o remount,size=1G /run/archiso/cowspace
-df -l
 cp copy/pacman.conf /etc/pacman.conf
-pacman -Sy reflector dialog --noconfirm
+pacman -Sy dialog --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 sh sh/format.sh
