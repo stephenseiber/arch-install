c8729a37c6a241eb62838cc935b8298e1f04b127
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-08-01 00:17:29 -0600

    Update format1.sh

--- sh/format1.sh
+++ sh/format1.sh
@@ -1,1 +1,19 @@
-
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
+parted --script "${device}" --  mklabel gpt \
+  mkpart ESP fat32 1Mib 275MiB \
+  set 1 boot on \
+  mkpart primary ext4 275MiB 100%
+part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?2$")"
+mkfs.vfat -F32 "${part_boot}"
+mkfs.ext4 -f "${part_root}"
+mount "${part_root}" /mnt
+mkdir /mnt/boot
+mount "${part_boot}" /mnt/boot
+echo desired swap size in Mib
+read swap
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=$swap status=progress
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
