30ac3c44aab6c7b77f27a0c468231c8f6034f1a1
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-08-01 00:52:10 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,7 +1,7 @@
 mount -o remount,size=10G /run/archiso/cowspace
 df -l
 cp copy/pacman.conf /etc/pacman.conf
-pacman -Sy reflector --noconfirm
+pacman -Sy reflector dialog --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 reflector --latest 10 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
