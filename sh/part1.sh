7547bb2ad5ca58afcd4cf4e6326b48727cd2382e
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-02 00:17:07 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,6 +1,7 @@
 cp copy/pacman.conf /etc/pacman.conf
 reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Sy dialog --noconfirm
+pacman -Sy dialog pacredir --noconfirm
+systemctl start pacredir
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
 sh sh/format.sh
