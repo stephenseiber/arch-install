7cf0b92571772c8f9b43e3469f92364768151904
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-02 11:46:53 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,5 +1,6 @@
 cp copy/pacman.conf /etc/pacman.conf
-reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+# uncomment line below if running first time on only machine in a lan
+#reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy dialog --noconfirm
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
