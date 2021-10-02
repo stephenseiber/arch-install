0422dc6b1c0ed7dd213a205c8566bb6bf0886411
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-02 00:25:55 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,5 +1,5 @@
 cp copy/pacman.conf /etc/pacman.conf
-reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+#reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy dialog pacredir --noconfirm
 systemctl start pacredir
 ls /sys/firmware/efi/efivars
