d62613dd053e7730e739a20bf6f8935342a9c54c
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-04 00:08:15 -0600

    Update yay.sh

--- sh/yay.sh
+++ sh/yay.sh
@@ -1,2 +1,17 @@
 useradd -m -g users -G wheel -s /bin/fish yay
 su yay
+mkdir -p /tmp/yay
+cd /tmp/yay
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
+mkdir -p /tmp/epson
+cd /tmp/epson
+git clone https://aur.archlinux.org/epson-inkjet-printer-escpr.git
+cd epson-inkjet-printer-escpr/
+makepkg -si --noconfirm
+mkdir -p /tmp/ttf-ms-fonts
+cd /tmp/ttf-ms-fonts
+git clone https://aur.archlinux.org/ttf-ms-fonts.git
+cd ttf-ms-fonts/
+makepkg -si --noconfirm
