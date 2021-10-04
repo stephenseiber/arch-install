effe62ef8035d0367d25f95773b3a698d79b6a53
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-03 19:35:59 -0600

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,5 +1,5 @@
 cp copy/pacman.conf /etc/pacman.conf
-# uncomment line below if running first time on only machine in a lan
+#uncomment line below if running first time on only machine in a lan
 #reflector --latest 13 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy dialog pacredir --noconfirm
 systemctl start pacredir
