677aac59e67433f2cfe49935d3899407ca009667
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-10-26 17:30:00 -0600

    Update user.sh

--- sh/user.sh
+++ sh/user.sh
@@ -9,7 +9,5 @@ passwd $user
 echo "what do you want root passwd to be"
 passwd
 echo passwd set
-useradd -m -g users -G wheel -s /bin/fish yay
-/bin/su -c "sh /yay.sh" - $yay
-userdel yay
-rm -r /home/yay
+
+
