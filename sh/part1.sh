608a0c447586194b1535c0b230d706ce2ad17fe5
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-11-12 02:29:47 -0700

    Update part1.sh

--- sh/part1.sh
+++ sh/part1.sh
@@ -1,7 +1,2 @@
-cp copy/pacman.conf /etc/pacman.conf
-reflector --latest 50 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist -c US --ipv6
-pacman -Sy dialog --noconfirm
-ls /sys/firmware/efi/efivars
-timedatectl set-ntp true
 sh sh/format.sh
 reboot
